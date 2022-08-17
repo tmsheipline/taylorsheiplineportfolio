100a9cd809671ce747a5dedaf23ae13b1de6c383
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-08-17 15:14:30 -0400

    react icon add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react'
+import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <Navbar />
+    </Router>
   )
 }
