e9e465a85f45fba8b24f86cc304cc260fb079a99
tmsheipline <taylorsheipline_2012@hotmail.com>
2022-08-26 18:44:11 -0400

    removed 2nd object fit

--- src/components/Hero/HeroElements.js
+++ src/components/Hero/HeroElements.js
@@ -27,7 +27,7 @@ export const VideoBg = styled.video`
     width: 100%;
     height: 100%;
     -o-object-fit: cover;
-    -o-object-fit: scale-down;
+    /* -o-object-fit: scale-down; */
     object-fit: cover;
     background: #232a34;
 `
